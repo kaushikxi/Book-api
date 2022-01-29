3d0a4ff00dfd4abb352a5792b92b0f5826f204d8
kaushikxi <kaushikq8@gmail.com>
2022-01-29 23:26:04 +0530

    initial

---
+++ requirement.js
@@ -0,0 +1,81 @@
+/*
+
+
+Requirements
+
+Book
+ -ISBN            - String
+ -Title           - String
+ -Author          - [Number]
+ -Language        - String
+ -Publications    - Number
+ -NumOfPages      - [String]
+ -Categories      - [String]
+
+
+ Author
+ -id              - Number
+ -name            - String
+ -books           - [String]
+
+
+
+ ---- APIS -----
+
+
+
+Book
+ -GET
+   - to get all books
+   - to get specific books
+   - to get a list of books based on category
+   - to get a list of books based on author
+
+
+ -POST
+    - to add new book
+ 
+ -PUT
+    - to update book details
+    - to update/add new author
+
+ -DELETE
+    - delete a book
+    - delete an author
+
+
+AUTHORS
+ -GET
+    - to get all authors
+    - to get specific author
+    - to get list of author based on book
+
+
+ -POST
+    - to add new author
+    - to update/add new author
+
+ -PUT
+    - update author details
+
+
+ -DELETE
+    - delete an author
+
+
+Publication
+ - GET
+    - to get all publication
+    - to get specific publication
+    - to get a list of publication on a specific book
+ - POST
+    - Add new publication
+ -PUT
+    - update publication
+    - to update/add new book
+ -DELETE
+    - delete a book from publication
+    - delete a publication
+
+
+*/
